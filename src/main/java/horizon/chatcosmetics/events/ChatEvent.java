c9c14c0c74e9620aa7dbfbb71e29008573861f80
Cervisial <44127813+DuncanCarr@users.noreply.github.com>
2020-05-15 09:41:41 -0500

    PAPI Implementation

--- src/main/java/horizon/chatcosmetics/events/ChatEvent.java
+++ src/main/java/horizon/chatcosmetics/events/ChatEvent.java
@@ -1,6 +1,7 @@
 package horizon.chatcosmetics.events;
 
 import horizon.chatcosmetics.ChatCosmetics;
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -9,10 +10,10 @@ public class ChatEvent implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
-        event.setFormat(ChatCosmetics.getInstance().colorize(ChatCosmetics.getInstance().getCosmeticsConfig().getConfig().getString("chat-format").replace("{NAME}", event.getPlayer().getDisplayName()).replace("{MESSAGE}", event.getMessage()).replace("{PREFIX}", ChatCosmetics.getChat().getPlayerPrefix(event.getPlayer())).replace("{SUFFIX}", ChatCosmetics.getChat().getPlayerSuffix(event.getPlayer()))));
+        event.setFormat(ChatCosmetics.getInstance().colorize(PlaceholderAPI.setPlaceholders(event.getPlayer(), ChatCosmetics.getInstance().getCosmeticsConfig().getConfig().getString("chat-format").replace("{NAME}", event.getPlayer().getDisplayName()).replace("{MESSAGE}", event.getMessage()).replace("{PREFIX}", ChatCosmetics.getChat().getPlayerPrefix(event.getPlayer())).replace("{SUFFIX}", ChatCosmetics.getChat().getPlayerSuffix(event.getPlayer())))));
         if (ChatCosmetics.getInstance().getCosmetics().containsKey(event.getPlayer().getUniqueId())) {
             event.setMessage(ChatCosmetics.getInstance().colorize(ChatCosmetics.getInstance().getCosmetics().get(event.getPlayer().getUniqueId()).getChatColor() + event.getMessage()));
-            event.setFormat(ChatCosmetics.getInstance().colorize(ChatCosmetics.getInstance().getCosmeticsConfig().getConfig().getString("chat-format").replace("{NAME}", ChatCosmetics.getInstance().getCosmetics().get(event.getPlayer().getUniqueId()).getNameColor() + event.getPlayer().getDisplayName()).replace("{MESSAGE}", event.getMessage()).replace("{PREFIX}", ChatCosmetics.getChat().getPlayerPrefix(event.getPlayer())).replace("{SUFFIX}", ChatCosmetics.getChat().getPlayerSuffix(event.getPlayer()))));
+            event.setFormat(ChatCosmetics.getInstance().colorize(PlaceholderAPI.setPlaceholders(event.getPlayer(), ChatCosmetics.getInstance().getCosmeticsConfig().getConfig().getString("chat-format").replace("{NAME}", ChatCosmetics.getInstance().getCosmetics().get(event.getPlayer().getUniqueId()).getNameColor() + event.getPlayer().getDisplayName()).replace("{MESSAGE}", event.getMessage()).replace("{PREFIX}", ChatCosmetics.getChat().getPlayerPrefix(event.getPlayer())).replace("{SUFFIX}", ChatCosmetics.getChat().getPlayerSuffix(event.getPlayer())))));
         }
     }
 }
